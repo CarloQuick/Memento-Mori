2f6ea3ecd05460849714cdbd793cb151c7332e03
Carlo Quick <98628846+CarloQuick@users.noreply.github.com>
2025-04-12 15:20:20 +0900

    Author now in displayed quote

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,8 @@ import { getStoicQuote } from './lib/getStoicQuote';
 export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('memento-mori.mementoMori', async () => {
 		const quoteData = await getStoicQuote();
-		vscode.window.showInformationMessage(quoteData.quote);
+		const quote = quoteData.quote + " --" + quoteData.author;
+		vscode.window.showInformationMessage(quote);
 	});
 
 	context.subscriptions.push(disposable);
